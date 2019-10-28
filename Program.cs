c75dd19470da67ab6478a4b1cec24e443d135583
tamhuynh1234 <52303386+tamhuynh1234@users.noreply.github.com>
2019-10-28 21:47:50 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace SimpleFactoryPattern
+{
+    public interface IAnimal
+    {
+        void Speak();
+        void Action();
+    }
+
+    public class Dog : IAnimal
+    {
+        public void Speak()
+        {
+            Console.WriteLine("Dog says: Gau-Gau.");
+        }
+        public void Action()
+        {
+            Console.WriteLine("Dogs prefer barking...");
+        }
+    }
+
+    public class Tiger : IAnimal
+    {
+        public void Speak()
+        {
+            Console.WriteLine("Tiger says: Gao-Gao.");
+        }
+        public void Action()
+        {
+            Console.WriteLine("Tigers prefer hunting...");
+        }
+    }
+
+    public abstract class ISimpleFactory
+    {
+        public abstract IAnimal CreateAnimal();
+    }
+
+    public class SimpleFactory : ISimpleFactory
+    {
+        public override IAnimal CreateAnimal()
+        {
+            IAnimal intendedAnimal = null;
+
+            Console.WriteLine("Enter your choice(0 for Dog, 1 for Tiger)");
+            string b1 = Console.ReadLine();
+
+            int input;
+            if (int.TryParse(b1, out input))
+            {
+                Console.WriteLine("You have entered {0}", input);
+                switch (input)
+                {
+                    case 0:
+                        intendedAnimal = new Dog();
+                        break;
+                    case 1:
+                        intendedAnimal = new Tiger();
+                        break;
+                    default:
+                        Console.WriteLine("You must enter either 0 or 1");
+                        throw new ApplicationException(
+                            String.Format(
+                                " Unknown Animal cannot be instantiated"));
+                }
+            }
+            return intendedAnimal;
+        }
+    }
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** Simple Factory Pattern ***\n");
+
+            IAnimal preferredType = null;
+            ISimpleFactory simpleFactory = new SimpleFactory();
+
+            #region The code region that will vary based on users preference 
+            preferredType = simpleFactory.CreateAnimal();
+            #endregion
+
+            #region The codes that do not change frequently
+            preferredType.Speak();
+            preferredType.Action();
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+}
